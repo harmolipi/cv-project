96b3d2d8865892a72bd40b2a56c575f52237d17a
harmolipi <nikob381@gmail.com>
2021-12-02 12:47:42 -0500

    Added remove button

--- src/components/EducationalInstitution.js
+++ src/components/EducationalInstitution.js
@@ -5,11 +5,19 @@ import DateOrPresent from './DateOrPresent';
 class EducationalInstitution extends React.Component {
   render() {
     return (
-      <div>
-        <EditableText text="Name of school" />
-        <EditableText text="Title of study" />
-        <EditableText text="Date enrolled" inputType="date" />
-        <DateOrPresent text="Date graduated" workOrSchool="school" />
+      <div className="educational-institution">
+        <div className="school-info">
+          <EditableText text="Name of school" />
+          <EditableText text="Title of study" />
+          <EditableText text="Date enrolled" inputType="date" />
+          <DateOrPresent text="Date graduated" workOrSchool="school" />
+        </div>
+        <button
+          className="delete-school"
+          onClick={() => this.props.deleteSchool(this.props.schoolNum)}
+        >
+          Remove
+        </button>
       </div>
     );
   }
--- src/components/WorkExperience.js
+++ src/components/WorkExperience.js
@@ -5,12 +5,20 @@ import DateOrPresent from './DateOrPresent';
 class WorkExperience extends React.Component {
   render() {
     return (
-      <div>
-        <EditableText text="Company Name" />
-        <EditableText text="Position Title" />
-        <EditableText text="Work description" inputType="textarea" />
-        <EditableText text="Date started" inputType="date" />
-        <DateOrPresent text="Date left" workOrSchool="work" />
+      <div className="work-experience">
+        <div className="work-info">
+          <EditableText text="Company Name" />
+          <EditableText text="Position Title" />
+          <EditableText text="Work description" inputType="textarea" />
+          <EditableText text="Date started" inputType="date" />
+          <DateOrPresent text="Date left" workOrSchool="work" />
+        </div>
+        <button
+          className="delete-school"
+          onClick={() => this.props.deleteWork(this.props.workNum)}
+        >
+          Remove
+        </button>
       </div>
     );
   }
