ebef7e10f18c3cac30b13b5ac540c30e1301e84f
harmolipi <nikob381@gmail.com>
2021-12-04 04:24:12 -0500

    Added fontawesome icon

--- src/components/PreviewModeButton.js
+++ src/components/PreviewModeButton.js
@@ -1,14 +1,31 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 
 class PreviewModeButton extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      previewMode: false,
+    };
+    this.togglePreviewMode = this.togglePreviewMode.bind(this);
+    this.togglePreviewState = this.togglePreviewState.bind(this);
   }
 
   togglePreviewMode() {
     const previewElements = document.querySelectorAll('.hide-on-preview');
     previewElements.forEach((element) => {
       element.classList.toggle('hidden');
+    });
+    this.togglePreviewState();
+  }
+
+  togglePreviewState() {
+    this.setState((prevState) => {
+      return {
+        previewMode: !prevState.previewMode,
+      };
     });
   }
 
@@ -18,7 +35,12 @@ class PreviewModeButton extends React.Component {
         className="preview-mode-button text-sm bg-white border border-gray-500 text-gray-500 hover:bg-gray-500 hover:text-white text-center py-1 px-3 rounded"
         onClick={this.togglePreviewMode}
       >
-        Preview mode
+        {this.state.previewMode ? (
+          <FontAwesomeIcon icon={faEyeSlash} />
+        ) : (
+          <FontAwesomeIcon icon={faEye} />
+        )}{' '}
+        <span className="ml-1">Preview mode</span>
       </button>
     );
   }
