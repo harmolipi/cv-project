88ce0ca054905b9f66cd8bf1914d10994f764341
harmolipi <nikob381@gmail.com>
2021-12-07 19:57:50 -0500

    Changed classes to functional components

--- src/components/PreviewModeButton.js
+++ src/components/PreviewModeButton.js
@@ -1,49 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 
-class PreviewModeButton extends React.Component {
-  constructor(props) {
-    super(props);
+const PreviewModeButton = () => {
+  const [previewMode, setPreviewMode] = useState(false);
 
-    this.state = {
-      previewMode: false,
-    };
-    this.togglePreviewMode = this.togglePreviewMode.bind(this);
-    this.togglePreviewState = this.togglePreviewState.bind(this);
-  }
-
-  togglePreviewMode() {
+  const togglePreviewMode = () => {
     const previewElements = document.querySelectorAll('.hide-on-preview');
     previewElements.forEach((element) => {
       element.classList.toggle('hidden');
     });
-    this.togglePreviewState();
-  }
+    togglePreviewState();
+  };
 
-  togglePreviewState() {
-    this.setState((prevState) => {
-      return {
-        previewMode: !prevState.previewMode,
-      };
-    });
-  }
+  const togglePreviewState = () => {
+    setPreviewMode((prevPreviewMode) => !prevPreviewMode);
+  };
 
-  render() {
-    return (
-      <button
-        className="preview-mode-button text-sm bg-white border border-gray-500 text-gray-500 hover:bg-gray-500 hover:text-white text-center py-1 px-3 rounded"
-        onClick={this.togglePreviewMode}
-      >
-        {this.state.previewMode ? (
-          <FontAwesomeIcon icon={faEyeSlash} />
-        ) : (
-          <FontAwesomeIcon icon={faEye} />
-        )}{' '}
-        <span className="ml-1">Preview mode</span>
-      </button>
-    );
-  }
-}
+  return (
+    <button
+      className="preview-mode-button text-sm bg-white border border-gray-500 text-gray-500 hover:bg-gray-500 hover:text-white text-center py-1 px-3 rounded"
+      onClick={togglePreviewMode}
+    >
+      {previewMode ? (
+        <FontAwesomeIcon icon={faEyeSlash} />
+      ) : (
+        <FontAwesomeIcon icon={faEye} />
+      )}{' '}
+      <span className="ml-1">Preview mode</span>
+    </button>
+  );
+};
 
 export default PreviewModeButton;
